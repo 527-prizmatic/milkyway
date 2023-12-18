3b7e1d4d099670163ff8851c2742034db53e8838
MaxenceM <maxence.maurel0@gmail.com>
2023-12-18 13:14:53 +0100

    menu2

--- aeh/MainMenu.c
+++ aeh/MainMenu.c
@@ -2,8 +2,23 @@
 
 sfSprite* SpriteBackgroundMenu;
 sfTexture* TextureBackgroundMenu;
-sfVector2f pos = { 400.0f, 300.0f };
+sfVector2f pos = { 960.0f, 540.0f };
 
+sfSprite* SpritePlayMenu;
+sfTexture* TexturePlayMenu;
+sfVector2f posPlay = { 960.0f, 540.0f };
+sfFloatRect rectPlay;
+
+sfSprite* SpriteQuitterMenu;
+sfTexture* TextureQuitterMenu;
+sfVector2f posQuitter = { 700.0f, 450.0f };
+sfFloatRect rectQuitter;
+
+sfSprite* SpriteTitreMenu;
+sfTexture* TextureTitreMenu;
+sfVector2f posTitre = { 400.0f, 200.0f };
+
+sfVector2i mousePosMenu;
 
 void initMenu()
 {
@@ -13,6 +28,13 @@ void initMenu()
 	sfSprite_setOrigin(SpriteBackgroundMenu, vector2f(sfSprite_getGlobalBounds(SpriteBackgroundMenu).width / 2, sfSprite_getGlobalBounds(SpriteBackgroundMenu).height / 2));
 	sfSprite_setPosition(SpriteBackgroundMenu, pos);
 	return SpriteBackgroundMenu;
+
+	SpritePlayMenu = sfSprite_create();
+	TexturePlayMenu = sfTexture_createFromFile(TEXTURE_PATH"buttonStart.png", NULL);
+	sfSprite_setTexture(SpritePlayMenu, TexturePlayMenu, sfTrue);
+	sfSprite_setOrigin(SpritePlayMenu, vector2f(sfSprite_getGlobalBounds(SpritePlayMenu).width / 2, sfSprite_getGlobalBounds(SpritePlayMenu).height / 2));
+	sfSprite_setPosition(SpritePlayMenu, posPlay);
+	return SpritePlayMenu;
 }
 
 void updateMenu(sfRenderWindow* _window)
@@ -25,4 +47,8 @@ void displayMenu(sfRenderWindow* _window)
 	sfSprite_setOrigin(SpriteBackgroundMenu, vector2f(sfSprite_getGlobalBounds(SpriteBackgroundMenu).width / 2, sfSprite_getGlobalBounds(SpriteBackgroundMenu).height / 2));
 	sfSprite_setPosition(SpriteBackgroundMenu, pos);
 	sfRenderWindow_drawSprite(_window, SpriteBackgroundMenu, NULL);
+
+	/*sfSprite_setOrigin(SpritePlayMenu, vector2f(sfSprite_getGlobalBounds(SpritePlayMenu).width / 2, sfSprite_getGlobalBounds(SpritePlayMenu).height / 2));
+	sfSprite_setPosition(SpritePlayMenu, posPlay);
+	sfRenderWindow_drawSprite(_window, SpritePlayMenu, NULL);*/
 }
