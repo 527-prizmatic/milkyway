0d2efa583c3802a92178244ce617d91fc22b9498
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2023-12-18 13:11:47 +0100

    a

--- aeh/render.h
+++ aeh/render.h
@@ -4,4 +4,4 @@
 #include "SFML/Graphics.h"
 
 sfTexture* newTexture(char* _n);
-void setupSprite(sfTexture* _t, sfVector2f _o, sfVector2f _s, sfVector2f _p);
+void setupSprite(sfSprite* _sprite, sfTexture* _t, sfIntRect _r, sfVector2f _o, sfVector2f _s, sfVector2f _p);
