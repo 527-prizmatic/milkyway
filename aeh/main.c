bc6931b3cdf3788492cf8c3d6209bb5c119f8007
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2023-12-18 15:30:48 +0100

    The enemy hath been doneth

--- aeh/main.c
+++ aeh/main.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <Windows.h>
 
 #include "render.h"
 #include "tools.h"
 #include "vect.h"
+#include "player.h"
+#include "enemy.h"
 
 int main() {
+	///***  = = =  PREINIT  = = =  ***///
+	ShowWindow(GetConsoleWindow(), 0);
+
 	initTools();
 	initMenu();
+
 	sfVideoMode mode = { W_WINDOW, H_WINDOW, 2 };
 	sfRenderWindow* w = sfRenderWindow_create(mode, "Milky Way", sfNone, NULL);
+	sfRenderWindow_setFramerateLimit(w, TICKSPEED);
 
 	/* == BACKDROP TEXTURES == */
 	sfTexture* bg_1 = newTexture(PATH_TEXTURES"res_test_1.png");
 	sfTexture* bg_main = newTexture(PATH_TEXTURES"backgroundMainMenu.jpg");
 	sfTexture* aeh = newTexture(PATH_TEXTURES"a.png");
+	sfTexture* grr = newTexture(PATH_TEXTURES"grr.png");
 
+	/* == PLAYER == */
+	Player player;
+	initPlayer(&player, aeh);
+	Enemy enemy;
+	initEnemy(&enemy, grr, vector2f(500., 500.));
 
 	float tick = 0., tickExit = 0.;
 	sfEvent e;
@@ -43,7 +57,10 @@ int main() {
 			displayMenu(w);
 			renderBackdrop(w, bg_main);
 	//		renderBackdrop(w, bg_1);
-			renderSprite(w, NULL, aeh, TEX_RECT_NULL, vector2f(8., 8.), vectorSnap(vector2f(mousePos.x, mousePos.y), 8));
+	//		renderSprite(w, NULL, aeh, TEX_RECT_NULL, vector2f(8., 8.), vectorSnap(vector2f(mousePos.x, mousePos.y), 8));
+
+			playerUpdate(&player, w);
+			enemyUpdate(&enemy, w);
 
 			sfRenderWindow_display(w);
 		}
